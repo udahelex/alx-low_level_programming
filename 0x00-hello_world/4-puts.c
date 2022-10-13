50ee12503aeade6f62518087aa66739372917a28
udahelex <udahemukaleon2020@gmail.com>
2022-10-13 14:31:21 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+i#include <stdio.h>
+/**
+ * * main entry
+ * * return 0;
+ * *
+ * */
+   int main(void)
+   {
+	   puts("\"Programming is like building a multilingual puzzle");
+	   return (0);
+   }	   
