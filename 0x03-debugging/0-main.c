fab43795eec224e6eff4e235b9ec795419399eae
udahemuka lambert <113758051+udahelex@users.noreply.github.com>
2022-10-21 12:33:06 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,20 @@
-ii
+#include "main.h"
+
+
+/**
+ * main - Test function for postive or negative
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	int i;
+
+
+	i = 0;
+	positive_or_negative(i);
+
+
+	return (0);
+}
