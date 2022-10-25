4bc3e9a35ce45a4071a18db5b36d43d158db5dd2
udahemuka lambert <113758051+udahelex@users.noreply.github.com>
2022-10-25 20:55:30 +0200

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,13 @@
-iiii
+#include "main.h"
+#include "stdio.h"
+/**
+ * reset_to_98 - takes the pointer and updates the value
+ * @n: int to check
+ * owned By zachee
+ * Return: 0 is sccess
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
